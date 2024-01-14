bedbb6be4be8d20b30f7aaa4833186eabdcb4dc3
Claudio Carige <ccarige@gmail.com>
2024-01-14 15:43:18 -0300

    Created Credentials interface and update AppModule and AppRouting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './infra/infra-adapters/pages/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
