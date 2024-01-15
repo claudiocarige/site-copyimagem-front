d66534a6f6275b069755a9cd1de25c7b0480e0f9
Claudio Carige <ccarige@gmail.com>
2024-01-15 00:28:02 -0300

    Created List Clientes Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './infra/infra-adapters/pages/login/login.component';
 import { HomeComponent } from './infra/infra-adapters/pages/home/home.component';
+import { ListClientesComponent } from './core/adapters/components/list-clientes/list-clientes.component';
 
 const routes: Routes = [
   { path: '', redirectTo:'login', pathMatch:'full'},
   { path:'login', component:LoginComponent},
-  { path:'home', component: HomeComponent}
+  { path:'home', component: HomeComponent, children:[
+    { path:'list-cliente', component: ListClientesComponent}
+  ]}
 ];
 
 @NgModule({
---
+++ src/app/core/adapters/components/list-clientes/list-clientes.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit, ViewChild, inject } from '@angular/core';
+import { ClienteService } from 'src/app/core/use-cases/services/service-cliente/cliente.service';
+import { Cliente } from './../../../models/modelCliente';
+import { ClientePF } from 'src/app/core/models/modelClientePF';
+import { ClientePJ } from 'src/app/core/models/modelClientePJ';
+import { MatTableDataSource } from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+
+
+@Component({
+  selector: 'app-list-clientes',
+  templateUrl: './list-clientes.component.html',
+  styleUrls: ['./list-clientes.component.scss']
+})
+export class ListClientesComponent implements OnInit{
+
+  ELEMENT_DATA: (ClientePF | ClientePJ)[] = [];
+
+  displayedColumns: string[] = ['id', 'nomeCliente', 'email',
+                    'telefone','whatsapp','inicioContrato' , 'situacaoFinanceira','cpf', 'cnpj', 'acoes'];
+  dataSource = new MatTableDataSource<ClientePF | ClientePJ>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+
+  private clienteService = inject(ClienteService);
+
+
+  constructor(){}
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll() {
+    console.log('FINDALL PARA API PIPOCA in USERLIST');
+    this.clienteService.findAll().subscribe((data) => {
+      if (Array.isArray(data)) {
+        this.ELEMENT_DATA = data;
+      } else if (data) {
+        this.ELEMENT_DATA = [data];
+      }
+      console.log(data);
+      this.dataSource = new MatTableDataSource<Cliente>(this.ELEMENT_DATA);
+      if (this.paginator) {
+        this.dataSource.paginator = this.paginator;
+      }
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+}
